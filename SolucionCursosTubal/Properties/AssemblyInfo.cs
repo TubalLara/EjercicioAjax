97a3945a1937fc76d5862e9b85710df7f3cc51fe
TubalLara <tublargar@gmail.com>
2014-10-08 12:26:16 +0200

    ejercicio formularioa y bdd ajax

---
+++ SolucionCursosTubal/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SolucionCursosTubal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SolucionCursosTubal")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e662dff3-a92f-4e37-8491-583026a342c1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SolucionCursosTubal/scripts/ajax1.js
@@ -0,0 +1,43 @@
+﻿var url = "http://localhost:50039/api/cursos/";
+
+var borrarTabla = function() {
+    document.getElementById("contenido").removeChild(document.getElementById("tablaDatos"));
+
+
+};
+
+var pintarTabla = function(datos) {
+    var tabla = document.createElement("table");
+    tabla.setAttribute("id", "tablaDatos");
+    for (var i = 0; i < datos.lenght; i++) {
+
+        var fila = document.createElement("tr");
+        var c1 = document.createElement("td");
+        var c2 = document.createElement("td");
+        var c3 = document.createElement("td");
+
+        var t1 = document.createTextNode(datos[i].nombre);
+        var t2 = document.createTextNode(datos[i].duracion);
+        var t3 = document.createTextNode(datos[i].profesor);
+
+        c1.appenChild(t1);
+        c2.appenChild(t2);
+        c3.appenChild(t3);
+
+        fila.appendChild(c1);
+        fila.appendChild(c2);
+        fila.appendChild(c3);
+
+        tabla.appenChild(fila);
+
+    }
+    document.getElementById("contenido").appendChild(tabla);
+
+}
+
+var urlfinal = url + "/cursos";
+
+var ajax = XMLHttpRequest();
+ajax.open("GET", urlfinal);
+ajax.onreadystatechange
+
